bc069681953c8847005d579374af557a5041643f
Will Vining <wfvining@gmail.com>
2014-04-23 12:50:29 -0600

    hmmm... some thoughts...

--- Server.hs
+++ Server.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Server ( ) where
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
+import Control.Distributed.Process.Closure
 --import qualified Control.Distributed.Process.Backend.SimpleLocalnet as SLn
 import Control.Monad
 
@@ -13,16 +15,26 @@ import Messages
 
 -- There are some types that the server needs for internal messages 
 -- that should not be visible to other modules...
--- The server needs 
+-- The server needs
+
+data OpResult = IxChange 
 
 -- | The main server process.
 -- /path/ specifies the path to its data files
 server :: String -> Process ()
 server path = forever $ do
-  (Connect clientPid) <- expect
-  handlerPid <- spawnLocal requestHandler
-  send clientPid handlerPid
+  newIx <- receiveWait [match getRequest, match putRequest]
+  server path -- newIx
 
+-- | Handle a document get request
+getRequest :: GetDoc -> Process () -- ?? What should be in the Process here?
+getRequest (GetDoc docId sp) = undefined
+putRequest (PutDoc doc sp) = undefined
+
+-- Will need to be able to spawn servers on remote nodes.
+remotable ['server]
+
+{-
 -- | spawned to handle requests from clients.
 requestHandler :: Process ()
 requestHandler =
@@ -33,4 +45,4 @@ requestHandler =
           putRequest (PutDoc doc sp)= do
             -- TODO putDoc 
             sendChan sp Nothing
-
+-}
