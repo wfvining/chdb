3da204b839d4dacd34cf1d6dee3497546c8e1428
Will Vining <wfvining@gmail.com>
2014-04-24 11:24:49 -0600

    what?

--- Client.hs
+++ Client.hs
@@ -4,4 +4,27 @@ import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Control.Distributed.Process.Backend.SimpleLocalnet
 
-import 
+import Control.Monad (forever)
+
+import Messages
+import Document
+
+makeRequest :: String -> Process ()
+makeRequest reqStr = do
+  selfNode <- getLocalNode
+  let (request:args) = words reqStr
+  case request of
+    "get" -> 
+              
+
+dispatch :: String -> IO ()
+dispatch input = do
+  let com = head . words $ input
+  return ()
+
+main :: IO ()
+main = forever $ do
+         line <- getLine
+         dispatch line
+         runProcess makeRequest
+         
