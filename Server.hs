85562edad3f30e1d7b6fa086ab7f756f07e6613c
Will Vining <wfvining@gmail.com>
2014-04-29 11:24:18 -0600

    new slave code. sends DocUpdates to the master for each doc the slave has

--- Server.hs
+++ Server.hs
@@ -1,77 +1,84 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Server 
-    ( getServerPid
-    , getServerPids
-    , startServer 
-    , chdbPort
+    ( chdbPort
+    , chdbServerName
     , __remoteTable
     ) where
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Control.Distributed.Process.Closure
-import qualified Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Distributed.Process.Backend.SimpleLocalnet
+
 import Control.Monad
 
 import qualified Data.ByteString as BS
 import Data.Typeable
 import Data.Binary
-import Data.HashMap
+import qualified Data.HashMap as HM
+import Data.List
+
+import System.Directory
 
 import Document
 import Messages
 
 -- | An internal message passed from the slaves to the master
 --   at startup, or whenever a document changes.
-data DocUpdate = DocUpdate DocId DocRevision deriving (Typeable)
+data DocStat = DocStat DocId DocRevision deriving (Typeable)
 
-instance Binary DocUpdate where
-  put (DocUpdate did ver) = put did >> put ver
+instance Binary DocStat where
+  put (DocStat did ver) = put did >> put ver
   get = do
     did <- get
     ver <- get
-    return $ DocUpdate did ver
+    return $ DocStat did ver
+
+data DocUpdate = DocUpdate DocStat ProcessId deriving (Typeable)
+
+instance Binary DocUpdate where
+  put (DocUpdate ds pid) = put ds >> put pid
+  get = do
+    ds  <- get
+    pid <- get
+    return $ DocUpdate ds pid
 
 chdbPort = "55989"
+chdbPath = "/tmp/" -- Just temproary.
+chdbServerName = "chdbMaster"
 
-master :: HashMap DocId ProcessId -> Process ()
-master index = undefined 
+getDocList :: IO [DocStat]
+getDocList = do
+  dirc <- getDirectoryContents chdbPath
+  let docs = filter (isSuffixOf ".chdb") dirc
+  fmap (map toDocUpdate) $ forM docs decodeFile
+  where toDocUpdate :: Document -> DocStat
+        toDocUpdate doc = DocStat (docId doc) (revision doc)
+
+master :: HM.Map DocId (DocRevision, ProcessId) -> Process ()
+master index = undefined
+
+slave :: ProcessId -> Process ()
+slave mPid = forever $ do
+  n <- expect :: Process Int -- Just filling space
+  liftIO $ print n
+
+-- | initialize a slave process by collecing the list of documents
+-- | it has and sending them to the master. 
+initSlave :: ProcessId -> Process ()
+initSlave mPid = do
+  self  <- getSelfPid
+  docls <- liftIO $ getDocList
+  forM_ docls $ \du -> send mPid (DocUpdate du self)
+  slave mPid
+
+remotable ['initSlave] -- XXX: Does slave need to be remotable?
 
 initMaster :: [NodeId] -> Process ()
 initMaster slaves = do
   self <- getSelfPid
-  slavePids <- forM slaves $ \nid -> spawnLink nid ($(mkClosure 'slave) self) 
-  master
+  slavePids <- forM slaves $ \nid ->
+    spawnLink nid ($(mkClosure 'initSlave) self) 
+  master HM.empty
 
-slave :: ProcessId -> Process ()
-slave _ = do z <- expect :: (Process Int)
-             liftIO $ print "foo"
-             return ()
-
-handleDocUpdate :: DocUpdate -> Either ProcessId ProcessId
-handleDocUpdate = undefined
-
--- | The main server process.
--- /path/ specifies the path to its data files
-server :: String -> Process ()
-server path = forever $ do
-  newIx <- receiveWait [ match getRequest
-                       , match putRequest]--XXX how will this work with Filter?
-  server path -- newIx
-
--- | Handle a document get request
-getRequest :: GetDoc -> Process () -- ?? What should be in the Process here?
-getRequest (GetDoc docId sp) = undefined
-putRequest (PutDoc doc sp) = undefined
-
--- | Initialize the server, registering it on the local node as 
--- | chdbServer.
-initServer :: Process ()
-initServer = getSelfPid >>= register "chdbServer" >> server "foo"
-
-startServer :: Process ()
-startServer = undefined
-
--- Will need to be able to spawn servers on remote nodes.
-$(remotable ['slave])
