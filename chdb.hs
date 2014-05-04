640978b466ed5cf26e6d9ba4f78fbf5df6795916
Will Vining <wfvining@gmail.com>
2014-05-04 10:24:38 -0600

    more stuff in Main

--- chdb.hs
+++ chdb.hs
@@ -2,11 +2,23 @@ module Main where
 
 import System.Environment
 
+import Control.Distributed.Process.Node
+import Control.Distriubted.Process.Backend.SimpleLocalnet
+
+import Server
+
+nodes = ["127.0.0.1"]
+
 dispatch :: String -> IO ()
 dispatch "server" = do
   putStrLn "Starting server"
-  -- Run a server.
+  -- Initialize nodes
+  backend <- initializeBackend "127.0.0.1" chdbPort $ 
+             initRemotetable Server.__remoteTable
+  nodes <- get
+  forkProcess (initMaster slaveNodes)
 dispatch "" = do
+  putStrLn "starting chdb client"
   -- Run a client.
 
 main :: IO ()
