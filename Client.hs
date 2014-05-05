32abe303d987a73a3c962f9436556fe2f4215958
Will Vining <wfvining@gmail.com>
2014-05-05 14:29:18 -0600

    spawning a client process, discovering peers, and locating the chdbMaster Process

--- Client.hs
+++ Client.hs
@@ -1,44 +1,79 @@
-module Client where
+module Client 
+       ( clientShell 
+       , chdbClientPort )
+       where
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
-import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Distributed.Process.Backend.SimpleLocalnet as SLn
 
-import Control.Monad (forever)
+import Control.Monad (forever, forM_)
+
+import qualified Data.ByteString.Char8 as C8
 
 import Messages
 import Document
 import Server
 
--- XXX: Maybe this should be in a different module.
-chdbRemoteTable = initRemoteTable Server.__remoteTable
+chdbClientPort = "55991"
 
--- XXX: 
+getServerPid = undefined
+
 get :: Process ()
-get = do
-  srvr <- getServerPid
-  ref  <- monitor srvr
-  -- ...
-  unmonitor ref
+get = undefined
+
+put :: Process ()
+put = undefined
 
 -- | request sends a request to the server
-request :: [String] -> IO ()
+{-request :: [String] -> IO ()
 request ("get":args) = do
-  backend <- initializeBackend "127.0.0.1" chdbPort chdbRemoteTable
+  backend <- initializeBackend "127.0.0.1" chdbSlavePort chdbRemoteTable
   forkProcess 
+-}
+
+request :: [String] -> IO ()
+request ["get", docId] = undefined
+request ("put":args)
+  | length args == 2 = undefined -- attempt to store a new document.
+  | length args == 3 = undefined -- attempt to update an existing document.
+  | otherwise = showHelp
 
 showHelp :: IO ()
-showHelp = putStrLn "type \'bye\' to exit."
+showHelp = do
+  putStrLn "type \'bye\' to exit."
+  putStrLn "type \'get <docId>\' to retrieve a document"
+  putStrLn "type \'put <docId> [docVersion] <string>\' to store the string under docId. docVersion is required if updateing the document."
 
-clientShell :: IO ()
-clientShell = do
-  putStr "chdb> "
-  c@(command:_) <- fmap words getLine
-  case command of
-    "bye"  -> return ()
-    "help" -> showHelp >> clientShell
-    _      -> request c >> clientShell
+-- | send a whereis request to every node in the list, waiting for a response
+--   if no response is received within a reasonable time then 
+findChdbMaster :: [NodeId] -> Process (Maybe ProcessId)
+findChdbMaster nodes = do
+  forM_ nodes $ \nid -> whereisRemoteAsync nid chdbServerName
+  expectWhereisReply
+    where expectWhereisReply = do
+            reply <- expectTimeout 2000 -- XXX: not sure what the units are...
+            case reply of
+              Just (WhereIsReply chdbServerName (Just pid)) -> 
+                return $ Just pid
+              Just (WhereIsReply chdbServerName Nothing) -> 
+                expectWhereisReply
+              Nothing -> return Nothing
+
+shell :: [NodeId] -> Process ()
+shell nodes = do
+  (Just chdbMaster) <- findChdbMaster nodes
+  interactiveShell chdbMaster
+    where interactiveShell :: ProcessId -> Process ()
+          interactiveShell master = do
+            liftIO $ putStr "chdb> "
+            command <- fmap words $ liftIO getLine
+            liftIO $ request command -- XXX
+
+clientShell :: String -> IO ()
+clientShell host = do
+  backend <- initializeBackend host chdbClientPort initRemoteTable
+  node <- SLn.newLocalNode backend
+  peers <- SLn.findPeers backend 500
+  runProcess node (shell peers)
               
-
-
-         
--- chdb.hs
+++ chdb.hs
@@ -1,3 +1,21 @@
+-- | The amazing CloudHaskell Database!
+-- The main driver for chdb.
+-- To start chdb, fisrst start slave nodes by running
+-- 
+-- > ./chdb slave <ip address to listen on>
+-- 
+-- on each computer to be used as a slave. A single machine can host
+-- both a slave and a master node.
+-- 
+-- Once all slave nodes have been started, start chdb by running
+-- 
+-- > ./chdb master <ip address to listen on>
+-- 
+-- This will start slave processes on all the slave nodes, as well as
+-- the mater process on the node you execute it on.
+-- 
+-- Will Vining
+-- Cinco de Mayo 2014
 module Main where
 
 import System.Environment
@@ -6,6 +24,7 @@ import Control.Distributed.Process.Node
 import Control.Distributed.Process.Backend.SimpleLocalnet
 
 import Server
+import Client
 
 chdbRemoteTable = Server.__remoteTable initRemoteTable
 
@@ -14,13 +33,14 @@ dispatch :: [String] -> IO ()
 dispatch ["slave", host]  = 
   initializeBackend host chdbSlavePort chdbRemoteTable >>= startSlave
 dispatch ["master", host] = do
+  putStrLn "Starting server"
   backend <- initializeBackend host chdbMasterPort chdbRemoteTable
   startMaster backend initMaster
-  putStrLn "Starting server"
   -- todo, find slave NodeIds, spawn Server.master.
-dispatch [""] = do
+dispatch ["client", host] = do
   putStrLn "starting chdb client"
-  -- Run a client.
+  backend <- initializeBackend host chdbClientPort chdbRemoteTable
+  clientShell -- host
 
 main :: IO ()
 main = do 
